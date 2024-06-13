64d42a97694f8c794a1e6ca4eae46cf2ca7e92bb
Sumit Kumar <sumikumar@adobe.com>
2024-06-13 14:28:14 +0530

    updates for block

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,6 +1,7 @@
-export default function decorate(block) {
-    const [quoteWrapper] = block.children;
-    const blockquote = document.createElement('blockquote');
-    blockquote.textContent = quoteWrapper.textContent.trim();
-    quoteWrapper.replaceChildren(blockquote);
+// eslint-disable-next-line no-unused-vars
+function decorate(block) {
+  const [quoteWrapper] = block.children;
+  const blockquote = document.createElement('blockquote');
+  blockquote.textContent = quoteWrapper.textContent.trim();
+  quoteWrapper.replaceChildren(blockquote);
 }
